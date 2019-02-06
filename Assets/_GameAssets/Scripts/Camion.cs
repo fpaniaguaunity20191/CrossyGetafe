6009f2ddaa662c1be9975f7156fa05797c9354d5
Fernando Paniagua <fpaniagua.unity.2018.9@gmail.com>
2019-02-06 13:39:20 +0100

    Initial commit

---
+++ Assets/_GameAssets/Scripts/Camion.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camion : MonoBehaviour {
+    public Transform origin;
+    public float speed;
+    private void Awake()
+    {
+        transform.position = origin.position;
+    }
+    private void Start()
+    {
+        
+    }
+    void Update () {
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        transform.position = origin.position;
+    }
+}
---
+++ Assets/_GameAssets/Scripts/Camion2.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camion2 : MonoBehaviour {
+    public Transform startPoint;
+    public Transform endPoint;
+    public float speed;
+    float porcentaje = 0.0f;
+
+	void Start () {
+        transform.position = startPoint.position;
+	}
+	
+	void Update () {
+        porcentaje = porcentaje + Time.deltaTime*speed;
+        transform.position = Vector3.Lerp(startPoint.position, endPoint.position, porcentaje);
+        transform.rotation = Quaternion.Lerp(startPoint.rotation, endPoint.rotation, porcentaje);
+        if (porcentaje >= 1)
+        {
+            porcentaje = 0;
+        }
+	}
+}
